157b91896425cedf42d869021d75a784863f0b57
adsamcik <adsamcik@users.noreply.github.com>
2020-01-31 15:20:59 +0100

    Changed folder capitalization to lowercase

---
+++ data/instance/GenericReadable.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using CsvHelper.Configuration.Attributes;
+using LiteDB;
+using Newtonsoft.Json;
+
+namespace LastIRead.Data.Instance {
+	/// <summary>
+	///     Generic readable implementation for most reading materials.
+	/// </summary>
+	internal class GenericReadable : IReadable {
+		/// <summary>
+		///     Last progress
+		/// </summary>
+		[Ignore]
+		[JsonIgnore]
+		[BsonIgnore]
+		public IProgress LastProgress =>
+			History.Count > 0 ? History.Last() : new GenericProgress(DateTime.MinValue, 0);
+
+		public ObjectId Id { get; set; }
+		public string Title { get; set; }
+
+		[Optional] public double MaxProgress { get; set; }
+
+		[Optional] public bool Ongoing { get; set; }
+
+		[Optional] public bool Abandoned { get; set; }
+
+		/// <summary>
+		///     Last date this was read.
+		/// </summary>
+		[Ignore]
+		[JsonIgnore]
+		[BsonIgnore]
+		public DateTime LastRead => LastProgress.Date;
+
+		[Optional]
+		[JsonIgnore]
+		[BsonIgnore]
+		public double Progress {
+			get => LastProgress.Value;
+			set => LogProgress(value);
+		}
+
+
+		[Optional] [JsonProperty] public IList<IProgress> History { get; private set; } = new List<IProgress>();
+
+		public void IncrementProgress() {
+			LogProgress(Progress + 1);
+		}
+
+		public void LogProgress(double progress) {
+			if (!Ongoing && MaxProgress > 0) progress = Math.Min(MaxProgress, progress);
+
+			var newProgress = new GenericProgress(DateTime.Today, progress);
+			if (LastRead != newProgress.Date) {
+				History.Add(newProgress);
+			} else {
+				History[^1] = newProgress;
+			}
+		}
+	}
+}
---
+++ import/abstract/IDataImporter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LastIRead.Import {
+	/// <summary>
+	///     Interface for data importing
+	/// </summary>
+	public interface IDataImporter {
+		/// <summary>
+		///     Array of import extensions.
+		/// </summary>
+		public IEnumerable<string> ImportExtensions { get; }
+
+		/// <summary>
+		///     Converts file from given format to new format.
+		/// </summary>
+		/// <param name="file">File</param>
+		/// <returns>List of imported readables</returns>
+		Task<IEnumerable<IReadable>> Import(FileInfo file);
+	}
+}
