25a7c33644ef128752be0da29457c260f0ba6968
adsamcik <adsamcik@users.noreply.github.com>
2020-02-10 14:01:37 +0100

    Code cleanup

--- windows/settings/SettingsWindow.xaml.cs
+++ windows/settings/SettingsWindow.xaml.cs
@@ -1,9 +1,9 @@
+﻿using System.Windows;
 using LastIRead.windows.settings.pages;
-using System.Windows;
 
 namespace LastIRead.windows.settings {
 	/// <summary>
-	/// Interaction logic for SettingsWindow.xaml
+	///     Interaction logic for SettingsWindow.xaml
 	/// </summary>
 	public partial class SettingsWindow : Window {
 		public SettingsWindow() {
--- windows/settings/pages/LicensePage.xaml.cs
+++ windows/settings/pages/LicensePage.xaml.cs
@@ -3,30 +3,30 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using LastIRead.resources;
-using Markdown = Markdig.Wpf.Markdown;
+using Markdig.Wpf;
 
 namespace LastIRead.windows.settings.pages {
 	/// <summary>
-	/// Interaction logic for LicensePage.xaml
+	///     Interaction logic for LicensePage.xaml
 	/// </summary>
 	public partial class LicensePage : Page {
 		public LicensePage() {
 			InitializeComponent();
 
 			var document = Markdown.ToFlowDocument(LoadLicenses());
-			var brush = (SolidColorBrush)Application.Current.Resources["SystemBaseHighColorBrush"];
-			var secondaryBrush = (SolidColorBrush)Application.Current.Resources["SystemBaseMediumHighColorBrush"];
+			var brush = (SolidColorBrush) Application.Current.Resources["SystemBaseHighColorBrush"];
+			var secondaryBrush = (SolidColorBrush) Application.Current.Resources["SystemBaseMediumHighColorBrush"];
 			foreach (var block in document.Blocks) {
 				if (block.Foreground is SolidColorBrush blockBrush) {
 					if (blockBrush.Color == Colors.Black) {
 						block.Foreground = brush;
-					} else {
-						block.Foreground = secondaryBrush;
+						continue;
 					}
-				} else {
-					block.Foreground = secondaryBrush;
 				}
+
+				block.Foreground = secondaryBrush;
 			}
+
 			FlowReader.Document = document;
 		}
 
