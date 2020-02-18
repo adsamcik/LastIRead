633191747b21670135c74a32b9c47aca5a7f6459
adsamcik <adsamcik@users.noreply.github.com>
2020-02-18 19:12:13 +0100

    Reformat

--- app/windows/main/pages/FilterPage.xaml.cs
+++ app/windows/main/pages/FilterPage.xaml.cs
@@ -1,21 +1,21 @@
 using System;
-using System.Windows.Controls;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Controls;
 
 namespace LastIRead.windows.main.pages {
 	/// <summary>
-	/// Interaction logic for FilterPage.xaml
+	///     Interaction logic for FilterPage.xaml
 	/// </summary>
 	public partial class FilterPage : Page {
-		private readonly string[] filterItems = new string[] {
-				Filter.Reading.ToString(),
-				Filter.Dropped.ToString(),
-				Filter.Ongoing.ToString(),
-				Filter.Finished.ToString()
-			};
-
 		public readonly FilterData filterData;
+
+		private readonly string[] filterItems = {
+			Filter.Reading.ToString(),
+			Filter.Dropped.ToString(),
+			Filter.Ongoing.ToString(),
+			Filter.Finished.ToString()
+		};
 
 		public FilterPage(FilterData filterData) {
 			InitializeComponent();
@@ -28,7 +28,7 @@ namespace LastIRead.windows.main.pages {
 
 		private void SetHideComboBox(FilterData filterData) {
 			var selectedList = new List<string>();
-			for (int i = 0; i < filterItems.Length; i++) {
+			for (var i = 0; i < filterItems.Length; i++) {
 				if (filterData.filter.HasFlag(Enum.Parse<Filter>(filterItems[i]))) {
 					selectedList.Add(filterItems[i]);
 				}
@@ -38,7 +38,7 @@ namespace LastIRead.windows.main.pages {
 		}
 
 		public Filter GetHideComboBox() {
-			return (Filter)HideComboBox.SelectedItems.Cast<string>().Sum(x => (int)Enum.Parse<Filter>(x));
+			return (Filter) HideComboBox.SelectedItems.Cast<string>().Sum(x => (int) Enum.Parse<Filter>(x));
 		}
 	}
 
