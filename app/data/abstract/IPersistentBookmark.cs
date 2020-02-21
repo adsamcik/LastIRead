7edf14ff2a4352e70aca097ab226bf961b984f55
adsamcik <adsamcik@users.noreply.github.com>
2020-02-21 16:10:14 +0100

    Fixed null reference when LiteDb resolves Id

--- app/data/abstract/IPersistentBookmark.cs
+++ app/data/abstract/IPersistentBookmark.cs
@@ -1,5 +1,8 @@
 using LastIRead.data.database;
+using LiteDB;
 
 namespace LastIRead {
-	public interface IPersistentBookmark : IBookmark, IDatabaseItem { }
+	public interface IPersistentBookmark : IBookmark, IDatabaseItem {
+		public new ObjectId? Id { get; set; }
+	}
 }
