c9780c5d974da76154b8734ad4f46139ca07bd84
adsamcik <adsamcik@users.noreply.github.com>
2020-03-01 09:33:58 +0100

    Updated documentation

--- app/data/database/data/abstract/DatabaseCollection.cs
+++ app/data/database/data/abstract/DatabaseCollection.cs
@@ -26,36 +26,68 @@ namespace LastIRead.data.database {
 			Database.Dispose();
 		}
 
+		/// <summary>
+		/// 	Updates single item.
+		/// </summary>
+		/// <param name="item">Item.</param>
 		public void Update(T item) {
 			Collection.Update(item);
 		}
 
+		/// <summary>
+		/// 	Updates all items in a collection.
+		/// </summary>
+		/// <param name="itemEnumerable">Item collection (Enumerable).</param>
 		public void Update(IEnumerable<T> itemEnumerable) {
 			Collection.Update(itemEnumerable);
 		}
 
+		/// <summary>
+		/// 	Inserts single item.
+		/// </summary>
+		/// <param name="item">Item.</param>
 		public void Insert(T item) {
 			Collection.Insert(item);
 		}
 
+		/// <summary>
+		/// 	Inserts item collection.
+		/// </summary>
+		/// <param name="itemEnumerable">Item collection (Enumerable).</param>
 		public void Insert(IEnumerable<T> itemEnumerable) {
 			Collection.Insert(itemEnumerable);
 		}
 
+		/// <summary>
+		/// 	Removes single item.
+		/// </summary>
+		/// <param name="item">Item.</param>
+		public void Delete(T item) {
+			Collection.Delete(item.Id);
+		}
+
+		/// <summary>
+		/// 	Removes all items from a collection.
+		/// </summary>
+		/// <param name="itemEnumerable">Item collection (Enumerable).</param>
 		public void Delete(IEnumerable<T> itemEnumerable) {
 			foreach (var item in itemEnumerable) {
 				Delete(item);
 			}
 		}
 
-		public void Delete(T item) {
-			Collection.Delete(item.Id);
-		}
-
+		/// <summary>
+		/// 	Returns all items in a database.
+		/// </summary>
+		/// <returns>Item collection (Enumerable).</returns>
 		public IEnumerable<T> GetAll() {
 			return Collection.FindAll().ToArray();
 		}
 
+		/// <summary>
+		/// 	Upserts an item.
+		/// </summary>
+		/// <param name="item">Item.</param>
 		public void Upsert(T item) {
 			Collection.Upsert(item);
 		}
